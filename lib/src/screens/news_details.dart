d34ef02b6486472d6970ca3367c4f476a9f2d66f
ahmeddhus <ahmeddhus@gmail.com>
2020-01-15 17:50:24 +0200

    -

--- lib/src/screens/news_details.dart
+++ lib/src/screens/news_details.dart
@@ -1,16 +1,60 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:news/src/models/item_model.dart';
+import '../blocs/comments_provider.dart';
 
 class NewsDetails extends StatelessWidget {
   final int itemId;
 
   NewsDetails({this.itemId});
 
-  Widget build(context){
+  Widget build(context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Details'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemsWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Loading..');
+        }
+
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text(itemId.toString());
+            }
+
+            return buildTitle(itemSnapshot.data);
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
